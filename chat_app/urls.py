d4dfc3132fbcc25b03a95b41ce03cc3bb260db40
Sparsh S. Pradhan <sparshs.pradhan@192.168.1.2>
2025-01-18 20:24:52 +0530

    update

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -12,8 +12,9 @@
 # ]
 from django.urls import path
 from . import views
-
+from django.shortcuts import redirect
 urlpatterns = [
+    path('', lambda request: redirect('/login/')),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('chat/', views.chat_view, name='chat'),
